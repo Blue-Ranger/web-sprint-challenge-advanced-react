3110bf488b8395d358ba417266ab0550c0a2cbee
Blue-Ranger <53351056+Blue-Ranger@users.noreply.github.com>
2020-08-16 22:15:10 -0400

    testing and custom hooks not complete

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,19 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+  const { queryByText } = render(<CheckoutForm />);
+  const header = querybyText(/form/i);
+  expect(header).toBeInTheDocument();
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+  //arrange
+  //act
+  //assert
+
+  const { getByText, getByTestId } = render(<CheckoutForm />);
+});

8b5e30c3c3b4a0cb2322973f4e9064e627d45a91
Blue-Ranger <53351056+Blue-Ranger@users.noreply.github.com>
2020-10-12 15:36:07 -0400

    struggling to write passing test code

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -5,11 +5,30 @@ import CheckoutForm from "./CheckoutForm";
 // Write up the two tests here and make sure they are testing what the title shows
 
 test("form header renders", () => {
-  const { queryByText } = render(<CheckoutForm />);
-  const header = querybyText(/form/i);
+  const { getByText } = render(<CheckoutForm />);
+  const header = getByText(/form/i);
   expect(header).toBeInTheDocument();
 });
 
 test("form shows success message on submit with form details", () => {
   const { getByLabelText, getByTestId } = render(<CheckoutForm />);
+
+  const firstname = getByLabelText(/first name/i);
+  const lastname = getByLabelText(/last name/i);
+  const address = getByLabelText(/address/i);
+  const city = getByLabelText(/city/i);
+  const state = getByLabelText(/state/i);
+  const zipcode = getByLabelText(/zip/i);
+  const button = getByLabelText(/button/i);
+
+  fireEvent.change(firstname, { target: { value: "Mr. Fancy" } });
+  fireEvent.change(lastname, { target: { value: "Pants" } });
+  fireEvent.change(address, { target: { value: "123 SomePlacePlaza Ave" } });
+  fireEvent.change(city, { target: { value: "NonyaBusiness" } });
+  fireEvent.change(state, { target: { value: "Virginia" } });
+  fireEvent.change(zipcode, { target: { value: "12345" } });
+  fireEvent.click(button);
+
+  const success = getByTestId(/successMessage/i);
+  expect(success).toBeInTheDocument();
 });
